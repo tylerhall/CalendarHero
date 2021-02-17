4788ab8bb155295114195b4fcb7a4abcd9a62c86
Tyler Hall <rth@tyler.io>
2021-02-17 10:20:25 -0600

    Initial Commit

---
+++ CalendarHero/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CalendarHero
+//
+//  Created by Tyler Hall on 2/17/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
